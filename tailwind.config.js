51183aec6265b4d4772778515f2d44b83cd3d9cd
Divyadharshana5 <divyadharshana3@gmail.com>
2024-07-31 22:13:34 +0530

    updated the tailwindconfig.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,42 +10,42 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding:"15px",
+      padding: "15px",
     },
     screens: {
       sm: "640px",
       md: "768px",
       lg: "960px",
-      xl:"1200px",
-    
+      xl: "1200px",
     },
     fontfamily: {
-      primary:"var(--font-jetbrainsMono)",
+      primary: "var(--font-jetbrainsMono)",
     },
     extend: {
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
-          to:{height:"var(--radix-accordion-content-height"},
+          to: { height: "var(--radix-accordion-content-height" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height" },
-          to: { height: "0"},
+          to: { height: "0" },
+        },
       },
-  },
     },
     animation: {
       "accordion-down": "accordion-down 0.2s ease-out",
-      "accordion":"accordion-up 0.2s ease-out",
+      accordion: "accordion-up 0.2s ease-out",
     },
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    theme: {
+      extend: {
+        backgroundImage: {
+          "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+          "gradient-conic":
+            "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        },
       },
     },
+    plugins: [],
   },
-  plugins: [],
 };
