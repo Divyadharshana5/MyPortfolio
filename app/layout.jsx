10838db7f7492f28f75f72afeb2e006a686d2104
Divyadharshana5 <divyadharshana3@gmail.com>
2024-07-31 22:03:59 +0530

    update on tailwind.css

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import { JetBrain_Mono } from "next/font/google";
+import "./globals.css";
+
+const JetbrainMono = JetBrain_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"]
+  variable:'--font-jetbrainsMono',
+});
+
+export const metadata = {
+  title: "dharshana portfolio",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={jetbrainsMono.variable}>{children}</body>
+    </html>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding:"15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl:"1200px",
+    
+    },
+    fontfamily: {
+      primary:"var(--font-jetbrainsMono)",
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to:{height:"var(--radix-accordion-content-height"},
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height" },
+          to: { height: "0"},
+      },
+  },
+    },
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion":"accordion-up 0.2s ease-out",
+    },
   theme: {
     extend: {
       backgroundImage: {
