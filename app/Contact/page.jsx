d4a91e87488429d5c7ae624093fcd6dae9476669
Divyadharshana5 <divyadharshana3@gmail.com>
2024-09-09 20:46:19 +0530

    finished contact page

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -87,7 +87,7 @@ const Contact = () => {
                 return (
                   <li key={index} className="flex items-center gap-6">
                     <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
-                      <div className="text-[28px]">{item.icon}</div>
+                      <div className="text-[28px]">{item.icons}</div>
                     </div>
                     <div className="flex-1">
                       <p className="text-white/60">{item.title}</p>
